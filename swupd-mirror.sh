cd3c2af103abdd8d051f3c0c1833c0b04aa1796b
Sad Pencil <qh06@qq.com>
2020-11-20 19:37:01 +0800

    Update swupd-mirror.sh

--- swupd-mirror.sh
+++ swupd-mirror.sh
@@ -3,16 +3,28 @@ set -e
 
 target_dir="$PWD/mirror"
 upstream_server_url="https://cdn.download.clearlinux.org"
-latest_version=$(( "(curl -- "$upstream_server_url"/latest)" ))
+
+_version=v0.1
+echo "swupd-mirror $_version"
+echo "Get the metadata from the upstream server..."
+
+latest_version=$(( "$(curl -- "$upstream_server_url"/latest)" ))
+echo "Lastest version: $latest_version"
+
 manifest="$(curl -- "$upstream_server_url"/update/"$latest_version"/Manifest.MoM)"
 version=$(( "$(echo "$manifest" | grep "^version:" | head -n 1 | awk -F ':' '{print $2}')" ))
 minversion=$(( "$(echo "$manifest" | grep "^minversion:" | head -n 1 | awk -F ':' '{print $2}')" ))
 
+[ "$version" = "$latest_version" ] || ( echo "Version mismatch. " 1>&2 ; exit 1 )
+echo "Min version: $minversion"
 unset manifest
+unset version
+
+echo "Downloading files..."
 
 cd -- "$target_dir"
 
-names=(0 version "$version" "$minversion")
+names=(0 version "$latest_version" "$minversion")
 
 for name in "${names[@]}"
 do
