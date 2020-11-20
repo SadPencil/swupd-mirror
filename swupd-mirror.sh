5c6d5af993d12c228b7a52d90e60c02321bf4e6d
Sad Pencil <qh06@qq.com>
2020-11-20 19:27:38 +0800

    Update swupd-mirror.sh

--- swupd-mirror.sh
+++ swupd-mirror.sh
@@ -3,10 +3,12 @@ set -e
 
 target_dir="$PWD/mirror"
 upstream_server_url="https://cdn.download.clearlinux.org"
-latest_version=$(curl -- "$upstream_server_url"/latest)
-manifest=$(curl -- "$upstream_server_url"/update/"$latest_version"/Manifest.MoM)
-version=$(( $(echo "$manifest" | grep "^version:" | head -n 1 | awk -F ':' '{print $2}') ))
-minversion=$(( $(echo "$manifest" | grep "^minversion:" | head -n 1 | awk -F ':' '{print $2}') ))
+latest_version=$(( "(curl -- "$upstream_server_url"/latest)" ))
+manifest="$(curl -- "$upstream_server_url"/update/"$latest_version"/Manifest.MoM)"
+version=$(( "$(echo "$manifest" | grep "^version:" | head -n 1 | awk -F ':' '{print $2}')" ))
+minversion=$(( "$(echo "$manifest" | grep "^minversion:" | head -n 1 | awk -F ':' '{print $2}')" ))
+
+unset manifest
 
 cd -- "$target_dir"
 
