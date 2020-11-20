e34b46d8e344d22c453420af5913fae9cbf4dd24
Sad Pencil <qh06@qq.com>
2020-11-20 20:02:42 +0800

    Update swupd-mirror.sh

--- swupd-mirror.sh
+++ swupd-mirror.sh
@@ -8,17 +8,23 @@ _version=v0.1
 echo "swupd-mirror $_version"
 echo "get the metadata from the upstream server..."
 
-latest_version="$(( "$( curl -- "$upstream_server_url"/latest)" ))"
+latest_version=$( curl -- "$upstream_server_url"/latest | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*$//g' )
+[[ "$latest_version" == ?(-)+([0-9]) ]] || ( echo "unexpected content: latest_version is not a number." 1>&2 ; exit 1 ) || exit 1
 echo "latest version: $latest_version"
 
 manifest="$( curl -- "$upstream_server_url"/update/"$latest_version"/Manifest.MoM )"
-version="$(( "$( echo "$manifest" | grep "^version:" | head -n 1 | awk -F ':' '{print $2}')" ))"
-minversion="$(( "$( echo "$manifest" | grep "^minversion:" | head -n 1 | awk -F ':' '{print $2}')" ))"
+version=$( echo "$manifest" | grep "^version:" | head -n 1 | awk -F ':' '{print $2}' | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*$//g' )
+minversion=$( echo "$manifest" | grep "^minversion:" | head -n 1 | awk -F ':' '{print $2}' | sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*$//g' )
 
-[ "$version" = "$latest_version" ] || ( echo "version mismatch. " 1>&2 ; exit 1 ) || exit 1
-echo "min version: $minversion"
+[[ "$version" == ?(-)+([0-9]) ]] || ( echo "unexpected manifest: version is not a number." 1>&2 ; exit 1 ) || exit 1
+[[ "$minversion" == ?(-)+([0-9]) ]] || ( echo "unexpected manifest: minversion is not a number." 1>&2 ; exit 1 ) || exit 1
+
+[ "$version" = "$latest_version" ] || ( echo "unexpected manifest: version mismatch." 1>&2 ; exit 1 ) || exit 1
+
 unset manifest
 unset version
+
+echo "min version: $minversion"
 
 echo "download files..."
 
